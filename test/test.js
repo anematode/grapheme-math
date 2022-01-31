e2bc6f834f0c044da054e8ac9c7edfd937a9d9eb
Timothy Herchen <timothy.herchen@gmail.com>
2022-01-30 17:16:42 -0800

    hm

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ import { roundUp } from "../src/example.js"
 
 describe('roundUp', function() {
   it('should round up', function() {
-    expect(roundUp(0)).to.equal(0)
+    expect(roundUp(0)).to.equal(5e-324)
+
+
   });
 });
