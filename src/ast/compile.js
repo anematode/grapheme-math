31d058daa77d8d92e765c427037f9e570fabda0d
Timothy Herchen <timothy.herchen@gmail.com>
2022-03-15 12:13:31 -0700

    begin compile

--- src/ast/compile.js
+++ src/ast/compile.js
@@ -1,3 +1,5 @@
+import {ASTNode} from "./node.js"
+
 export class CompilationError extends Error {
   constructor (message) {
     super(message)
@@ -8,16 +10,28 @@ export class CompilationError extends Error {
 
 /**
  * Compile an ASTNode into an evaluable function or functions. This should be preferred for any expression that will be
- * evaluated many times.
- * @param node
+ * evaluated many times. This function should be pretty well optimized...
+ * @param root
  * @param modes
  */
-export function compileNode(node, {
+export function compileNode(root, {
   modes = ["normal"],
-  exportedVariables = ["x", "y"],
-  constants = {}
+  exportedVariables = [],
+  staticVariables = []
 } = {}) {
-  if (!node.allResolved()) {
+  if (!(root instanceof ASTNode)) {
+    throw new CompilationError("First argument to compileNode must be an ASTNode")
+  }
+
+  if (!root.allResolved()) {
     throw new CompilationError("Node types must be resolved with .resolveTypes() first")
   }
+
+  // Deduced information about nodes
+  let nodeInformation = new Map()
+  root.applyAll(node => {
+    nodeInformation.set(node, {})
+  })
+
+  return nodeInformation
 }
