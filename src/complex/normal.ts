52bf438f4548a3cf157ba00ff6f2928c31045409
Timothy Herchen <timothy.herchen@gmail.com>
2022-03-20 18:17:45 -0700

    add types to complex/normal.ts

--- src/complex/normal.ts
+++ src/complex/normal.ts
@@ -1,18 +1,24 @@
 import {gammaReal} from "../real/normal.js"
 import {lanczosCoefficients} from "../real/gamma.js"
 
+/**
+ * Normal-precision complex number.
+ */
 class Complex {
+  re: number;
+  im: number;
+
   constructor (re=0, im=0) {
     this.re = +re
     this.im = +im
   }
 
   /**
-   * Attempt to parse an object (returns an undefined Complex if couldn't sensibly convert)
+   * Attempt to parse an object. Returns a Complex with NaN parts if couldn't sensibly convert.
    * @param o {any}
    * @return {Complex}
    */
-  static fromObj (o) {
+  static fromObj (o: any): Complex {
     let re = NaN, im = NaN
     if (typeof o === "number") {
       re = o
@@ -20,7 +26,7 @@ class Complex {
     } else if (typeof o === "object") {
       if (Array.isArray(o)) {
        re = +o[0]
-       im = +g[1]
+       im = +o[1]
       } else if (o.x !== undefined && o.y !== undefined) {
         re = +o.x
         im = +o.y
@@ -33,50 +39,58 @@ class Complex {
     return new Complex(re, im)
   }
 
-  add (c1, c2) {
-    this.re = c1.re + c2.re
-    this.im = c1.im + c2.im
+  /**
+   * Add two complex numbers and write the result to this complex number.
+   * @param c1
+   * @param c2
+   */
+  add (c1: Complex, c2: Complex) {
+    let c1r = c1.re, c2r = c2.re, c1i = c1.im, c2i = c2.im
+    this.re = c1r + c2r
+    this.im = c1i + c2i
   }
 
-  multiply (c1, c2) {
+  multiply (c1: Complex, c2: Complex) {
     let c1r = c1.re, c2r = c2.re, c1i = c1.im, c2i = c2.im
     this.re = c1r * c2r - c1i * c2i
     this.im = c1r * c2i + c1i * c2r
   }
 
-  multiplyReal (c, r) {
+  multiplyReal (c: Complex, r: number) {
     this.re = c.re * r
     this.im = c.im * r
   }
 
-  divide (c1, c2) {
+  divide (c1: Complex, c2: Complex) {
     let c1r = c1.re, c2r = c2.re, c1i = c1.im, c2i = c2.im
-    let denom = 1 / (c2r * c2r + c2i * c2i)
-    this.re = (c1r * c2r + c1i * c2i) * denom
-    this.im = (c1i * c2r - c1r * c2i) * denom
+    let d = 1 / (c2r * c2r + c2i * c2i)
+
+    this.re = (c1r * c2r + c1i * c2i) * d
+    this.im = (c1i * c2r - c1r * c2i) * d
   }
 
-  reciprocal (c) {
+  reciprocal (c: Complex) {
     let cr = c.re, ci = c.im
     let denom = 1 / (cr * cr + ci * ci)
+
     this.re = cr * denom
     this.im = -ci * denom
   }
 
-  subtract (c1, c2) {
+  subtract (c1: Complex, c2: Complex) {
     let c1r = c1.re, c2r = c2.re, c1i = c1.im, c2i = c2.im
     this.re = c1r - c2r
     this.im = c1i - c2i
   }
 
-  sin (c) {
+  sin (c: Complex) {
     let cr = c.re, ci = c.im
 
     this.re = Math.sin(cr) * Math.cosh(ci)
     this.im = Math.cos(cr) * Math.sinh(ci)
   }
 
-  cos (c) {
+  cos (c: Complex) {
     let cr = c.re, ci = c.im
 
     this.re = Math.cos(cr) * Math.cosh(ci)
@@ -102,11 +116,12 @@ class Complex {
 
   log (c) {
     let cr = c.re, ci = c.im
+
     this.re = Math.log(Math.hypot(c.re, ci))
     this.im = Math.atan2(c.im, cr)
   }
 
-  pow (c1, c2) {
+  pow (c1: Complex, c2: Complex) {
     let c2i = c2.im
 
     if (c2i === 0) {
@@ -127,7 +142,7 @@ class Complex {
    * @param b {Complex}
    * @param r {number}
    */
-  powReal (b, r) {
+  powReal (b: Complex, r: number) {
     if (Number.isInteger(r)) {
       if (r === 0) { // TODO add more integer handling
         this.re = 1
@@ -144,15 +159,15 @@ class Complex {
     this.exp(c)
   }
 
-  static abs (c) {
+  static abs (c: Complex): number {
     return Math.hypot(c.re, c.im) // kinda slow, but whatevs
   }
 
-  static absSquared (c) {
+  static absSquared (c: Complex): number {
     return c.re * c.re + c.im * c.im
   }
 
-  static arg (c) {
+  static arg (c: Complex): number {
     return Math.atan2(c.im, c.re)
   }
 
@@ -162,7 +177,7 @@ class Complex {
    * @param c {Complex}
    * @returns {number}
    */
-  static approxArg(c) {
+  static approxArg (c: Complex): number {
     // Credit to https://math.stackexchange.com/a/1105038/677124
     let x = c.im, y = c.re
 
@@ -180,7 +195,7 @@ class Complex {
     return r
   }
 
-  gamma (z) {
+  gamma (z: Complex) {
     let zi = z.im, zr = z.re
     if (zi < 1e-15) {
       this.re = gammaReal(zr)
