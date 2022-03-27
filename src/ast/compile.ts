4f06f14f73a7dd88784e884050f6991fb073340c
Timothy Herchen <timothy.herchen@gmail.com>
2022-03-27 02:49:03 -0700

    begone

--- src/ast/compile.ts
+++ src/ast/compile.ts
@@ -10,142 +10,8 @@ export class CompilationError extends Error {
   }
 }
 
-function analyzeNode (root, infoMap) {
-  root.applyAll(node => {
-    // For each
-  })
-}
-
-function logVariableLocations(variableLocations, log) {
-  for (let [ varName, varInfo ] of variableLocations.entries()) {
-    let location = varInfo.location
-
-    switch (location) {
-      case "evaluate":
-        log(() => `Variable ${varName} is to be evaluated (${varInfo.constant ? "constant" : "nonconstant"})`)
-        break
-      case "static":
-        log(() => `Variable ${varName} is static`)
-        break
-      case "scope":
-        log(() => `Variable ${varName} is to be found in the scope object`)
-        break
-      case "input":
-        log(() => `Variable ${varName} is to be found at index ${varInfo.index}`)
-        break
-    }
-  }
-}
-
-function getVariableLocations(staticVariables, usedVariables, inputFormat, log, doDebug) {
-  /**
-   * Map between variables and where to find them
-   * @type {Map<string, { location: string }>}
-   */
-  let variableLocations = new Map()
-
-  // Whether a scope is used
-  let scopeIndex = inputFormat.indexOf('scope')
-  let usesScope = scopeIndex !== -1
-
-  if (usesScope) {
-    let dup = inputFormat.indexOf('scope', scopeIndex + 1)
-    if (dup !== -1) {
-      throw new CompilationError(`Scope is defined twice, at indices ${scopeIndex} and ${dup}`)
-    }
-
-    variableLocations.set("scope", { // scope is a special variable, kinda...
-      location: "input",
-      mangledName: "scope",
-      index: scopeIndex
-    })
-  }
-
-  for (let i = 0; i < inputFormat.length; ++i) {
-    let v = inputFormat[i]
-
-    if (typeof v !== 'string') {
-      throw new CompilationError(`Input variable at index ${i} is not a string`)
-    }
-
-    if (v === "scope") continue
-    if (!usedVariables.get(v)) {
-      throw new CompilationError(`Input variable ${v} at index ${i} is unused`)
-    }
-
-    variableLocations.set(v, {
-      location: "input",
-      mangledName: v,   // don't mangle input variables
-      index: i
-    })
-  }
-
-  for (let [ varName, varInfo ] of usedVariables.entries()) {
-    let definition = varInfo.operatorDefinition
-    let existingLocation = variableLocations.get(varName)
-
-    if (definition) {  // variable is to be evaluated, as in a builtin variable
-      if (existingLocation) {
-        // TODO descriptive
-        throw new CompilationError(`Variable ${varName} is a defined constant`)
-      }
-
-      variableLocations.set(varName, {
-        location: "evaluate",
-        operatorDefinition: definition,
-        mangledName: genVariableName(),  // mangle to avoid clashes
-        constant: definition.tags.constant
-      })
-    } else {
-      if (existingLocation) continue
-
-      if (usesScope) {
-        // If a scope is used, assume the variable lies in the scope
-        variableLocations.set(varName, {
-          location: "scope",
-          mangledName: varName  // okay to not mangle this
-        })
-
-        continue
-      }
-
-      let staticInfo = staticVariables.get(varName)
-      if (staticInfo) {
-        variableLocations.set(varName, {
-          location: "static",
-          mangledName: genVariableName(),
-          info: staticInfo
-        })
-      } else {
-        // If all else fails, assume static with no information
-        variableLocations.set(varName, {
-          location: "static",
-          mangledName: genVariableName(),
-          info: null
-        })
-      }
-    }
-  }
-
-  if (doDebug) {
-    logVariableLocations(variableLocations, log)
-  }
-
-  return {
-    usesScope,
-    variableLocations
-  }
-}
-
-function checkInputFormat(inputFormat) {
-  if (!Array.isArray(inputFormat)) {
-    if (typeof inputFormat !== "string") {
-      throw new CompilationError("inputFormat must be an array or string")
-    }
-
-    inputFormat = [ inputFormat ]
-  }
-
+// Throws if an input format is invalid (i.e., if there are two arguments with the same name)
+function checkInputFormat(inputFormat: Array<string>) {
   for (let i = 0; i < inputFormat.length; ++i) {
     let varName = inputFormat[i]
 
@@ -155,191 +21,17 @@ function checkInputFormat(inputFormat) {
       }
     }
   }
-
-  return inputFormat
 }
 
 let id = 0
 
 /**
- * Generate a unique variable name that will not conflict with other names
+ * Generate a unique variable name that will not conflict with other names (locally or globally; no name will ever be
+ * returned twice)
  * @returns {string}
  */
-function genVariableName () {
+function genVariableName (): string {
   return "$" + (++id)
-}
-/**
- *
- * @param inputFormat
- * @param variableLocations
- * @param usesScope
- * @param typechecks
- */
-function getVariableRetrieval (inputFormat, variableLocations, usesScope, typechecks) {
-  let signature = inputFormat
-  let fragment = new CodeFragment()
-
-  if (usesScope && typechecks) {
-    // scope typecheck must precede all others
-    let scopeIndex = variableLocations.get("scope").index
-    fragment.insertFragment(getScopeTypecheck(scopeIndex))
-  }
-
-  for (let [ varName, varInfo ] in variableLocations.entries()) {
-    let location = varInfo.location
-    switch (location) {
-      case "evaluate":
-      case "static":
-        // Will be evaluated or set separately from the main function body (in the preamble)
-        fragment.insertFragment(new SpecialFragment("static_variable", varInfo))
-        break
-      case "scope":
-        fragment.insertFragment(`let ${varInfo.mangledName} = scope.${varName};`)
-        break
-      case "input": // Will be set no matter what
-        break
-      default:
-        throw new CompilationError("?")
-    }
-
-    fragment.defines.push(varInfo.mangledName)
-  }
-
-  return { signature, fragment }
-}
-// Write a function which checks whether a string is a valid JS variable name (used for detecting accidental stuff, not
-// sanitizing). $ is not allowed.
-function isValidVariable (name) {
-  return typeof name === "string" && /^[a-zA-Z_][a-zA-Z0-9_]*$/.test(name)
-}
-
-function checkVariableNames (variableLocations) {
-  for (let varName in variableLocations.values()) {
-    if (!isValidVariable(varName)) {
-      throw new CompilationError(`Invalid variable name ${varName}`)
-    }
-  }
-}
-
-function resolveConcreteTypes (root, nodeInformation, mode) {
-  for (let [ node, info ] of nodeInformation.entries()) {
-    let concreteType = mode.getConcreteType(node.type)
-
-    if (!concreteType) {
-      throw new CompilationError(`Unable to find concrete type for mathematical type ${node.type} in mode ${mode.name}`)
-    }
-
-    info.concreteType = concreteType
-  }
-}
-
-function resolveConstants (root, nodeInformation, mode) {
-  let fragment = new CodeFragment()
-
-
-}
-
-function resolveEvaluators (root, nodeInformation, mode) {
-  // Bottom up; extra
-  root.applyAll(node => {
-    let info = nodeInformation.get(node)
-    let evaluator = null
-    let cast = null, def
-
-    if (def = node.operatorDefinition) {
-      evaluator = def.getBestEvaluator({
-
-      })
-    }
-
-    info.evaluator = evaluator
-  }, false, true /* children first */)
-}
-
-function compileTarget (root, target, opts) {
-  if (typeof target !== "object") {
-    throw new CompilationError(`Target description must be a JS object, not ${typeof target}`)
-  }
-
-  const doDebug = opts.debug ?? true
-  let debugLog = []
-  let globalNodeInformation = opts.nodeInformation
-
-  // For debugging purposes
-  function log (msg) {
-    if (!doDebug) return
-    if (typeof msg === "function") { // avoid unnecessary string computations when not debugging
-      msg = msg()
-    }
-
-    debugLog.push(msg)
-  }
-
-  let mode = target.mode
-  if (mode === undefined) {
-    log("Evaluation mode not given; defaulting to normal")
-    mode = "normal"
-  }
-
-  try {
-    mode = toEvaluationMode(target.mode)
-  } catch (e) {
-    // Re-raise error
-    throw new CompilationError(e.message)
-  }
-
-  let inputFormat = target.inputFormat
-  inputFormat = checkInputFormat(inputFormat)
-
-  // Compute variable locations; a variable is either provided as an input directly, provided in the scope object,
-  // an operator definition, or (if all fails) a static variable. Non-builtin variables are presumed to be in the scope
-  // if not explicitly declared to be static
-  let staticVariables = opts.staticVariables ?? []
-  let usedVariables = opts.usedVariables
-  let typechecks = opts.typechecks ?? true
-
-  let { variableLocations, usesScope } = getVariableLocations(staticVariables, usedVariables, inputFormat, log, doDebug)
-  checkVariableNames(variableLocations)
-
-  // The evaluation procedure is as follows:
-  //  - Get all variables from their respective locations
-  //  - If enabled, typecheck and convert variables
-  //  - Compute each node in sequence, abstracted as a set of assignments
-  //  - Return the result
-
-  let assnGraph = generateAssignmentGraph(root, variableLocations, usesScope, opts)  // Elides identity casts and single-element ASTGroups
-
-
-
-  return {
-    mode,
-    assnGraph,
-    debug: debugLog
-  }
-
-  let nodeInformation = createInformationMap(root)
-  resolveConcreteTypes(root, nodeInformation, mode)  // TODO: prefer the simplest possible type regardless of mode
-
-  // Some constants may need special processing, for example in an arbitrary-precision setting
-  let constantFragment = resolveConstants(root, nodeInformation, mode)
-
-  // Resolves evaluators and concrete casts. Most nodes, and every non-leaf node, will have an evaluator and a set of
-  // concrete casts (possibly identity casts)
-  resolveEvaluators(root, nodeInformation, mode)
-
-  // At this point, everything is (pretty much) an evaluator, besides variables. Constants are to be computed via
-  // calling concreteType.castPermissive on their value. Writes evaluators are preferred over new evaluators. Casts
-  // are also evaluators.
-
-  //let { signature, fragment: variableRetrievalFragment } = getVariableRetrieval(inputFormat, variableLocations, usesScope, typechecks)
-
-  //let assignmentGraph = getAssignmentGraph()
-
-  return {
-    mode,
-    nodeInformation,
-    debug: debugLog
-  }
 }
 
 function createInformationMap(root) {
@@ -447,6 +139,7 @@ function fillTargetOptions(nodeOpts: CompileNodeOptions, opts: CompileTargetOpti
   if (p !== -1) {
     throw new CompilationError(`Provided variable name at index ${p} in input format is not a string`)
   }
+  checkInputFormat(inputFormat)
   if (!Array.isArray(staticVariables)) {
     throw new CompilationError(`Static variables must be an array`)
   }
