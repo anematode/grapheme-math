91743def45b45bb8daad661606704e29feef17f3
Timothy Herchen <timothy.herchen@gmail.com>
2022-04-04 11:16:09 -0700

    colour

--- src/complex/domain_coloring.ts
+++ src/complex/domain_coloring.ts
@@ -1,45 +1,20 @@
 import {Complex} from "./normal.js"
-
-type RGB = [ number, number, number ]
-
-function hue2rgb(p, q, t) {
-    if (t < 0) t += 1
-    else if (t > 1) t -= 1
-
-    if (t < 1 / 6) return p + (q - p) * 6 * t;
-    if (t < 1 / 2) return q;
-    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-
-    return p;
-}
-
-function hslToRGB(h: number, s: number, l: number): RGB {
-    let r, g, b;
-
-    if (s === 0) {
-        r = g = b = l; // achromatic
-    } else {
-        let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        let p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
-    }
-
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
-
-export function complexToRGB (c: Complex): RGB {
-  let h = (Complex.arg(c) + 2 * Math.PI / 3) / (2 * Math.PI)
-  let s = 1
-  let l = 2 / Math.PI * Math.atan(Complex.abs(c))
-
-  return hslToRGB(h, s, l)
-}
+import { Color } from "../other/color.js";
 
 function fastAtan (x: number): number { // positive x only
     if (x > 1) return (Math.PI / 2 - fastAtan(1 / x))
     return (Math.PI / 4) * x - x * (x - 1) * (0.2447 + 0.0663 * x);
+}
+
+// Method of converting complex to RGBA
+abstract class ColoringScheme {
+    writeComplexArrayToRGBA: (c: Complex[], arr: Uint8ClampedArray, index: number, colorScale: number) => void;
+
+    writeComplexToRGBA (c: Complex, arr: Uint8ClampedArray, index: number, colorScale: number) {
+        this.writeComplexArrayToRGBA([c], arr, index, colorScale)
+    }
+
+    complexToColor: (c: Complex) => Color;
 }
 
 export function writeComplexToRGBA (c: Complex, arr: Uint8ClampedArray, index: number, colorScale: number) {
