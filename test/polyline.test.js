33f1ba23d069e8deccc7782648f98c4ffcce6ecc
Timothy Herchen <timothy.herchen@gmail.com>
2022-04-25 08:09:41 -0700

    impl later

--- test/polyline.test.js
+++ test/polyline.test.js
@@ -1,2 +1,8 @@
 // Test various polyline-related algorithms
 
+function validatePolyline (pVertices, triangleStrip, pen) {
+  // Essentially, we choose various random vertices surrounding the polyline, compute their distances to the polyline,
+  // and, if they're close enough, make sure they are within the triangle strip
+
+
+}
