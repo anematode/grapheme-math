b6be80537e0038c95f8f099a50e05f50fb182399
Timothy Herchen <timothy.herchen@gmail.com>
2022-04-25 08:29:38 -0700

    vec2 array conversion

--- src/core/renderer_instruction.ts
+++ src/core/renderer_instruction.ts
@@ -1,5 +1,22 @@
+import { Pen } from "../other/pen.js";
 
-type RendererInstruction = {}
+type BaseInstruction = {
+  zIndex: number
+}
+
+export type PolylineRendererInstruction = BaseInstruction & {
+  insnType: "polyline",
+  // Flattened array of vertices; typed array is preferred
+  vertices: Float32Array | Float64Array | number[],
+  pen: Pen
+}
+
+export type DebugInstruction = BaseInstruction & {
+  insnType: "debug",
+  // Flattened array of vertices; typed array is preferred
+}
+
+export type RendererInstruction = PolylineRendererInstruction
 
 export type RenderingInfo = {
   instructions: RendererInstruction[]
--- src/vec/vec2.ts
+++ src/vec/vec2.ts
@@ -1,4 +1,12 @@
-// Another one of these, yada yada, reinventing the wheel, yay
+import { isTypedArray } from "../utils";
+
+type CardinalDirection = "N" | "NE" | "E" | "SE" | "S" | "SW" | "W" | "NW" | "C"
+export type Vec2Like = { x: number, y: number } | [ number, number ] | Vec2 | { im: number, re: number } | CardinalDirection
+export type Vec2ArrayLike = Float32Array | Float64Array | Vec2[] | number[]
+
+/**
+ * Generic Vec2 class, reinventing the wheel...
+ */
 export class Vec2 {
   x: number
   y: number
@@ -8,6 +16,10 @@ export class Vec2 {
     this.y = y
   }
 
+  /**
+   * Permissively convert to Vec2 from an arbitrary object, ideally something that is Vec2Like
+   * @param obj
+   */
   static fromObj (obj: unknown) {
     let x: number = NaN,
       y: number = NaN
@@ -95,4 +107,85 @@ export class Vec2 {
   lengthSquared () {
     return this.x * this.x + this.y * this.y
   }
+}
+
+function throwInconvertibleElement(elem: any, i: number): never {
+  throw new TypeError(`Expected input to be convertible to a flat array of Vec2s, found element ${elem} at index ${i}`)
+}
+
+function throwOddLengthArray(len: number): never {
+  throw new Error(`Expected input to be convertible to a flat array of Vec2s, got array of odd length ${len}`)
+}
+
+function vec2NonFlatArrayConversion (arr, f32 = true, throwOnError=false) {
+  let ret = new (f32 ? Float32Array : Float64Array)(arr.length * 2)
+  let retIndex = -1
+
+  for (let i = 0; i < arr.length; ++i) {
+    let elem = arr[i]
+
+    if (elem.x) {
+      ret[++retIndex] = elem.x
+      ret[++retIndex] = elem.y
+    } else if (Array.isArray(elem)) {
+      if (elem.length !== 2) {
+        if (throwOnError)
+          throwInconvertibleElement(elem, i)
+
+        return
+      }
+
+      ret[++retIndex] = elem[0]
+      ret[++retIndex] = elem[1]
+    } else {
+      if (throwOnError)
+        throwInconvertibleElement(elem, i)
+
+      return
+    }
+  }
+
+  return ret
+}
+
+/**
+ * Flatten a given array relatively permissively into an flat array; returns undefined if the conversion failed
+ * @param obj
+ * @param f32
+ * @param throwOnError
+ */
+export function vec2ArrayConversion (obj, f32 = true, throwOnError=false): Float32Array | Float64Array | undefined {
+  if (Array.isArray(obj)) {
+    for (let i = 0; i < obj.length; ++i) {
+      if (typeof obj[i] !== 'number') {
+        return vec2NonFlatArrayConversion(obj)
+      }
+    }
+
+    // Obj is just an array of numbers
+    if (obj.length % 2 === 1) {
+      if (throwOnError)
+        throwOddLengthArray(obj.length)
+
+      return
+    }
+
+    return new (f32 ? Float32Array : Float64Array)(obj)
+  } else if (isTypedArray(obj)) {
+    if (obj.length % 2 === 1) {
+      if (throwOnError)
+        throwOddLengthArray(obj.length)
+
+      return
+    }
+
+
+    if (f32 && obj instanceof Float32Array) return obj
+    if (!f32 && obj instanceof Float64Array) return obj
+
+    return new (f32 ? Float32Array : Float64Array)(obj)
+  }
+
+  if (throwOnError)
+    throw new TypeError(`Could not convert object into flat Vec2 array`)
 }
