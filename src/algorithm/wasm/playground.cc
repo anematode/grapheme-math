6561bc3f5902d0eb4898b3f93e249ed2ae7e6da5
Timothy Herchen <timothy.herchen@gmail.com>
2022-04-27 14:31:06 -0700

    fix typos

--- src/algorithm/wasm/playground.cc
+++ src/algorithm/wasm/playground.cc
@@ -2,28 +2,30 @@
 
 extern "C" {
   void bounding_box_flat_f32(float* start, int len) {
+    len >>= 2;
+
     float xmin = std::numeric_limits<float>::infinity();
     float ymin = xmin;
     float xmax = -xmin;
     float ymax = xmax;
-    
-    for (int i = 0; i < len; ++i) {
+
+    for (int i = 0; i < len; i += 2) {
       float x = start[i];
       float y = start[i + 1];
-      
+
       xmin = x > xmin ? xmin : x;
       ymin = y > ymin ? ymin : y;
       xmax = x < xmax ? xmax : x;
       ymax = y < ymax ? ymax : y;
     }
-    
+
     auto write = (volatile float*) nullptr;
-    
+
     write[0] = xmin;
     write[1] = ymin;
     write[2] = xmax;
     write[3] = ymax;
-    
+
     // Please the rest of the code
     write[4] = xmin;
     write[5] = ymin;
